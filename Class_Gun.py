168fec9d58191e5c16e37cbc7578b3e79e3c1046
viska0319@icloud.com <mata_@localhost.localdomain>
2023-10-13 13:28:28 +0900

    총기 이미지 출력 방식 변경 및 스프라이트 수정

--- Class_Gun.py
+++ Class_Gun.py
@@ -9,18 +9,17 @@ class Gun:
         self.scar_right = load_image(scar_h_right_directory)
         self.scar_left = load_image(scar_h_left_directory)
         self.p = p
-        self.deg_right, self.deg_left = 0, 0
+        self.deg = 0
 
     def draw(self):
         if GUN_NAME == 'SCAR_H':  # GUN_NAME에 따라 사용하는 총기가 달라진다.
             if self.p.dir == 1:
-                self.scar_right.rotate_draw(self.deg_right, self.p.x, self.p.y - self.p.land_y, 170, 120)
+                self.scar_right.clip_composite_draw(0, 0, 150, 100, self.deg, '', self.p.x, self.p.y - self.p.land_y, 170, 120)
             elif self.p.dir == 0:
-                self.scar_left.rotate_draw(self.deg_left, self.p.x, self.p.y - self.p.land_y, 170, 120)
+                self.scar_left.clip_composite_draw(0, 0, 150, 100, self.deg, 'h, v', self.p.x, self.p.y - self.p.land_y, 170, 120)
 
     def update(self):
-        self.deg_right = math.atan2(self.p.my - self.p.y, self.p.mx - self.p.x)
-        self.deg_left = math.atan2(self.p.my - self.p.y, self.p.mx - self.p.x)
+        self.deg = math.atan2(self.p.my - self.p.y, self.p.mx - self.p.x)
 
     def handle_event(self, event):
         pass
