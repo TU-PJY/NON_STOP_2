a163f7739ea2e16925fca73c70521a13682a3316
TU-PJY <viska0319@icloud.com>
2023-11-14 14:43:13 +0900

    장탄수 출력 코드 간소화

--- ui_manager/ingame_manager.py
+++ ui_manager/ingame_manager.py
@@ -10,23 +10,22 @@ def load_resource(self):
 
 
 def render_ammo_ind(self):
-	if self.weapon.reload_need:  # 총알을 모두 소모하면 인디케이터가 붉은색으로 표시된다
-		self.g, self.b = 0, 0
-	else:
-		self.r, self.g, self.b = 255, 255, 255
+	cur = self.weapon.cur_ammo
 
 	if self.weapon.gun_type == 'pistol' or self.weapon.gun_type == 'smg':
-		self.font.draw(20, 120, '%d | %d' % (self.weapon.cur_ammo, self.weapon.num_ammo_small),\
-		 (self.r, self.g, self.b))
+		num = self.weapon.num_ammo_small
 	elif self.weapon.gun_type == 'ar':
-		self.font.draw(20, 120, '%d | %d' % (self.weapon.cur_ammo, self.weapon.num_ammo_middle),\
-		 (self.r, self.g, self.b))
+		num = self.weapon.num_ammo_middle
 	elif self.weapon.gun_type == 'rifle' or self.weapon.gun_type == 'sr':
-		self.font.draw(20, 120, '%d | %d' % (self.weapon.cur_ammo, self.weapon.num_ammo_big),\
-		 (self.r, self.g, self.b))
-		
+		num = self.weapon.num_ammo_big
+
+	self.font.draw(20, 120, '%d | %d' %(cur, num), (self.r, self.g, self.b))
 	pass
 
 
 def update_ammo_ind(self):
+	if self.weapon.reload_need:  # 총알을 모두 소모하면 인디케이터가 붉은색으로 표시된다
+		self.g, self.b = 0, 0
+	else:
+		self.r, self.g, self.b = 255, 255, 255
 	pass
